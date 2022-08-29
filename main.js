88ae41479156a167d6b980ac9cb2f35d165230be
Airyclon <84323073+Airyclon@users.noreply.github.com>
2022-08-29 16:40:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+video = "";
+status = "";
+object = "";
+
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480, 380);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status- Detecting Objects";
+    object = document.getElementById("input").value;
+    console.log(object);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
